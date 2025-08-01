6e0148152019c9b4bb42fa67512a356f822c035e
Joshua Carlson <61999256+jrcarl624@users.noreply.github.com>
2025-08-01 16:02:41 -0400

    Update tests/integration.rs
    Co-authored-by: arexon <arexonreal@gmail.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,17 +1,3 @@
-pub fn format_filename(s: &str) -> String {
-    // Windows sucks
-    // <>:"/\|?*
-    s.replace("<", "")
-        .replace(">", "")
-        .replace(":", "")
-        .replace("/", "")
-        .replace("\\", "")
-        .replace("|", "")
-        .replace("?", "")
-        .replace("*", "")
-        .replace("\n", "")
-}
-
 mod codegen;
 
 mod parser;
