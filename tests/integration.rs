c92f02b4d020b8eb5c87fb9fe34c25f4208d24d6
arexon <arexonreal@gmail.com>
2025-09-13 23:44:04 +0300

    move transformer tests to files

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,7 +1,7 @@
 use std::{fs, path::Path};
 
 use insta::Settings;
-use nolana::Parser;
+use nolana::{Codegen, CodegenOptions, MolangTransformer, Parser};
 
 fn with_settings(f: impl FnOnce()) {
     let mut settings = Settings::clone_current();
@@ -16,11 +16,27 @@ fn read_and_parse(path: &Path) -> String {
     format!("{result:#?}")
 }
 
+fn read_and_transform(path: &Path) -> String {
+    let source = fs::read_to_string(path).unwrap();
+    let mut result = Parser::new(&source).parse();
+    MolangTransformer::default().transform(&mut result.program);
+    Codegen::default().with_options(CodegenOptions { minify: false }).build(&result.program)
+}
+
 #[test]
 fn test_parser() {
     with_settings(|| {
         insta::glob!("parser/*.nolana", |path| {
             insta::assert_snapshot!(read_and_parse(path));
+        });
+    });
+}
+
+#[test]
+fn test_transformer() {
+    with_settings(|| {
+        insta::glob!("transformer/*.nolana", |path| {
+            insta::assert_snapshot!(read_and_transform(path));
         });
     });
 }
