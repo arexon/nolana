64805c0c328e4bcae272fef7e165402030e73b86
arexon <arexonreal@gmail.com>
2025-09-14 01:20:43 +0300

    refactor(parser): remove `panicked` field from `ParseResult` (#52)
    Addresses #40. `ParseResult.panicked` is set to `true` when at least one
    error has occurred, which isn't entirely accurate. But since Nolana has
    no notion of "non-recoverable errors", having this field provides
    nothing of value to the user as `ParseResults.errors` already includes
    the encountered errors.

--- examples/full.rs
+++ examples/full.rs
@@ -9,15 +9,12 @@ use nolana::{
 fn main() {
     let source_text = fs::read_to_string("examples/sample.molang").unwrap();
 
-    let ParseResult { mut program, errors, panicked } = Parser::new(&source_text).parse();
+    let ParseResult { mut program, errors } = Parser::new(&source_text).parse();
 
     if !errors.is_empty() {
         for error in errors {
             let error = error.with_source_code(source_text.clone());
             print!("{error:?}");
-        }
-        if panicked {
-            println!("Encountered an unrecoverable error");
         }
         return;
     }
--- examples/parser.rs
+++ examples/parser.rs
@@ -5,15 +5,12 @@ use nolana::{ParseResult, Parser};
 fn main() {
     let source_text = fs::read_to_string("examples/sample.molang").unwrap();
 
-    let ParseResult { program, errors, panicked } = Parser::new(&source_text).parse();
+    let ParseResult { program, errors } = Parser::new(&source_text).parse();
 
     if !errors.is_empty() {
         for error in errors {
             let error = error.with_source_code(source_text.clone());
             print!("{error:?}");
-        }
-        if panicked {
-            println!("Encountered an unrecoverable error");
         }
         return;
     }
