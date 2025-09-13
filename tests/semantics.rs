dbe64453a31968fd7631424fe5d6841a73bfc351
arexon <arexonreal@gmail.com>
2025-09-14 00:48:31 +0300

    test: move test cases to `.nolana` files (#50)
    Addresses #48 by creating specialized `.nolana` files for every test
    case. Tests now live in a single `/tests/integration.rs` module.

--- tests/semantics.rs
+++ tests/semantics.rs
@@ -8,66 +8,6 @@ fn semantics(source: &str) -> String {
 }
 
 #[test]
-fn empty_block_expression() {
-    let out = semantics("{}");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn filled_block_expression() {
-    let out = semantics("{1;};");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn illegal_string_operation_both() {
-    let out = semantics("'foo' + 'bar'");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn illegal_string_operation_left() {
-    let out = semantics("'foo' == 1");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn illegal_string_operation_right() {
-    let out = semantics("1 + 'bar'");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn unequals_string_operation() {
-    let out = semantics("'bar' != 'bar'");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn equals_string_operation() {
-    let out = semantics("'bar' == 'bar'");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn assigning_context() {
-    let out = semantics("context.foo = 0;");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn break_outside_loop() {
-    let out = semantics("break;");
-    assert_snapshot!(out)
-}
-
-#[test]
-fn break_inside_loop() {
-    let out = semantics("loop(1, {break;});");
-    assert_snapshot!(out)
-}
-
-#[test]
 fn continue_outside_loop() {
     let out = semantics("continue;");
     assert_snapshot!(out)
